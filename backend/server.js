207b94fe05a71c26b3d717832b69dfeefcaea94f
Petar Kanev (A1 BG) <Petar.Kanev@a1.bg>
2023-10-17 14:23:14 +0300

    combined log

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-import chalk from "chalk";
-import cookieParser from "cookie-parser";
 import "dotenv/config";
+import chalk from "chalk";
 import express from "express";
+import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import cors from "cors";
+// since we are using ES modules - we have to add .js extension
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 
 const app = express();
-console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -17,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+app.use(morganMiddleware);
+
 app.get("/api/v1/test", (req, res) => {
   res.json({ Hi: "Hello from the Invoice app!!!!" });
 });
@@ -24,10 +28,12 @@ app.get("/api/v1/test", (req, res) => {
 const PORT = process.env.PORT || 1997;
 
 app.listen(PORT, () => {
-  // console.log("Server is running!");
   console.log(
     `${chalk.green.bold("✔")} 👍 Server is running in ${chalk.yellow.bold(
       process.env.NODE_ENV
     )} mode on port ${chalk.blue.bold(PORT)}`
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
 });
